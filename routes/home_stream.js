4adeef259b4eb251cfe0a5a50f8e7865a1570859
Chanh Le <giaosudau@gmail.com>
2013-07-29 00:30:44 +0700

    Get Home and Get More in HomeStream

--- routes/home_stream.js
+++ routes/home_stream.js
@@ -7,103 +7,98 @@
  */
 
 exports.get = function (request, response) {
-    Dishes.find().limit(4).sort('-created_at').exec(function (err, docs) {
-        if (err) response.json("error", error)
-        else {
-            res = []
-            _.each(docs, function (item) {
-                item.populate('created_by', 'name screen_name avatar')
-                    .populate('ingredients')
-                    .populate('categories')
-                    .populate('comments', function (err, dishes) {
-                        if (err) response.json('error', err)
-                        else
-                            Comment.populate(item, {
-                                path: 'comments.created_by',
-                                select: 'name screen_name avatar',
-                                model: 'User'
-                            }, function (err, result) {
-                                logger.info(result)
-                                res.push(result)
-                                if (res.length == docs.length) {
-                                    response.json({'info': 'success', 'dishes': res})
-                                }
-                            });
 
+    LoginToken.checkTokenIsExpired(request.body.name, request.body.token, request.body.device, function (info) {
+        console.log("Check Token: ", info);
+        if (info == "success") {
+            Dishes.find().limit(10).sort('-created_at').exec(function (err, docs) {
+                if (err) response.json("error", error)
+                else {
+                    res = []
+                    _.each(docs, function (item) {
+                        item
+                            .populate('created_by', 'name screen_name avatar')
+                            .populate('ingredients')
+                            .populate('categories')
+                            .populate('comments', function (err, dishes) {
+                                if (err) response.json('error', err)
+                                else
+                                    Comment.populate(item, {
+                                        path: 'comments.created_by',
+                                        select: 'name screen_name avatar',
+                                        model: 'User'
+                                    }, function (err, result) {
+                                        logger.info(result)
+                                        res.push(result)
+                                        if (res.length == docs.length) {
+                                            response.json({'info': 'success', 'dishes': res})
+                                        }
+                                    });
+
+                            })
                     })
-            })
-            logger.warn(res)
+                    logger.warn(res)
 
+                }
+            })
+
+        } else {
+            console.log(info);
+            response.json(info);
         }
-    })
+    });
+
+
 }
 
 exports.getMore = function (request, response) {
-    var created_at = resquest.body.create_at;
-    logger.info(created_at)
-    Dishes.find()
-        .where('create_at').lte(created_at)
-        .sort('-created_at')
-        .limit(5)
-        .exec(function (err, docs) {
-            if (err) response.json("error", error)
-            else {
-                res = []
-                _.each(docs, function (item) {
-                    item.populate('created_by', 'name screen_name avatar')
-                        .populate('ingredients')
-                        .populate('categories')
-                        .populate('comments', function (err, dishes) {
-                            if (err) response.json('error', err)
-                            else
-                                Comment.populate(item, {
-                                    path: 'comments.created_by',
-                                    select: 'name screen_name avatar',
-                                    model: 'User'
-                                }, function (err, result) {
-                                    logger.info(result)
-                                    res.push(result)
-                                    if (res.length == docs.length) {
-                                        response.json({'info': 'success', 'dishes': res})
-                                    }
-                                });
+    var created_at = request.body.created_at;
+    if(!created_at){
+        response.json({"error" :'created-at-missed'})
+    }
+    LoginToken.checkTokenIsExpired(request.body.name, request.body.token, request.body.device, function (info) {
+        console.log("Check Token: ", info);
+        if (info == "success") {
+            Dishes.find()
+                .where('created_at')
+                .lte(created_at)
+                .sort('-created_at')
+                .limit(10)
+                .exec(function (err, docs) {
+                    if (err) response.json("error", err)
+                    else {
+                        res = []
+                        _.each(docs, function (item) {
+                            item
+                                .populate('created_by', 'name screen_name avatar')
+                                .populate('ingredients')
+                                .populate('categories')
+                                .populate('comments', function (err, dishes) {
+                                    if (err) response.json('error', err)
+                                    else
+                                        Comment.populate(item, {
+                                            path: 'comments.created_by',
+                                            select: 'name screen_name avatar',
+                                            model: 'User'
+                                        }, function (err, result) {
+                                            logger.info(result)
+                                            res.push(result)
+                                            if (res.length == docs.length) {
+                                                response.json({'info': 'success', 'dishes': res})
+                                            }
+                                        });
 
+                                })
                         })
+                    }
+
                 })
-                logger.warn(res)
 
-            }
+        } else {
+            console.log(info);
+            response.json(info);
+        }
+    });
 
-        })
-    /*
-     Dishes.find().limit(100).sort('-created_at').exec(function (err, docs) {
-     if (err) response.json("error", error)
-     else {
-     res = []
-     _.each(docs, function (item) {
-     item.populate('created_by', 'name screen_name avatar')
-     .populate('ingredients')
-     .populate('categories')
-     .populate('comments', function (err, dishes) {
-     if (err) response.json('error', err)
-     else
-     Comment.populate(item, {
-     path: 'comments.created_by',
-     select: 'name screen_name avatar',
-     model: 'User'
-     }, function (err, result) {
-     logger.info(result)
-     res.push(result)
-     if(res.length == docs.length){
-     response.json({'info': 'success', 'dishes': res})
-     }
-     });
 
-     })
-     })
-     logger.warn(res)
-
-     }
-     })
-     */
 }
